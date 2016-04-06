927464dfa9fdf9525be4e736f5f9d5f8cf354b09
Mahdi Saber <mahdi.saber.de@gmail.com>
2016-04-06 21:29:41 +0200

    Revert "Adding method02 to Class01 to test revert - Step1"
    This reverts commit d81f04005e684c711c55cbe4e7b920576f697942.

--- workspace Git/P01/src/Class01.java
+++ workspace Git/P01/src/Class01.java
@@ -4,9 +4,5 @@ public class Class01
 	public void method01(){
 		System.out.println("This is a test for revert!");
 	}
-	
-	public void method02(){
-		System.out.println("This is a method to test revert!");
-	}
 
 }
