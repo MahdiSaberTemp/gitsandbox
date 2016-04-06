d81f04005e684c711c55cbe4e7b920576f697942
Mahdi Saber <mahdi.saber.de@gmail.com>
2016-04-06 20:53:02 +0200

    Adding method02 to Class01 to test revert - Step1

--- workspace Git/P01/src/Class01.java
+++ workspace Git/P01/src/Class01.java
@@ -4,5 +4,9 @@ public class Class01
 	public void method01(){
 		System.out.println("This is a test for revert!");
 	}
+	
+	public void method02(){
+		System.out.println("This is a method to test revert!");
+	}
 
 }
